f38c9138021bdbe00fa57e4ad90cb608a115c180
sickcell <17959575+p477d343@users.noreply.github.com>
2024-03-13 13:58:59 +0800

    Update pdp.py

--- pdp.py
+++ pdp.py
@@ -3,14 +3,18 @@ import requests
 
 app = Flask(__name__)
 
-POLICY_ENGINE_URL = "http://100.77.173.105:5002/evaluate-policies"
-
-@app.route('/access-decision', methods=['POST'])
-def access_decision():
+# Policy Decision Point (PDP)
+@app.route('/pdp', methods=['POST'])
+def pdp_request():
     request_data = request.data
-    response = requests.post(POLICY_ENGINE_URL, data=request_data)
-    decision = response.json()["decision"]
+    decision = policy_engine_request(request_data)
     return jsonify({"decision": decision})
 
+def policy_engine_request(request_data):
+    url = "http://100.77.173.105:5002/policy-engine"
+    headers = {"Content-Type": "application/octet-stream"}
+    response = requests.post(url, data=request_data, headers=headers)
+    return response.json()["decision"]
+
 if __name__ == '__main__':
-    app.run(host='100.77.173.105', port=5001, debug=True)
+    app.run(port=5001, debug=True)
